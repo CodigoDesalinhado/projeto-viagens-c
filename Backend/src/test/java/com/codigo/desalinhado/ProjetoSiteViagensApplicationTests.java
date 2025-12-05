5f2c1c43d91731306f264a9c4544a87b6eb513e8
marcelo0611 <mh0864622@gmail.com>
2025-12-05 10:52:47 -0300

    Adição do Backend

---
+++ Backend/src/test/java/com/codigo/desalinhado/ProjetoSiteViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.desalinhado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSiteViagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
